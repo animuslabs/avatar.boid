d554870699559c6044e24d5045e8f4d20e8b434d
piecesnbits <piecesnbits@protonmail.com>
2022-02-24 17:27:38 +0000

    add ignore

--- util/.env.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    keys: {
-        jungle: ["5JJp4oPsm8sBa5xQ8AzFpbDpmW1hxweKdqabSrgrDQRtpjtaPYT"],
-        waxtest: ["5JYKenBWBQr5NguFqyjreFPMnEzLyqyzi6wi5JHkt1C6iGc9YSm"]
-    },
-    defaultChain: "waxtest"
-}
